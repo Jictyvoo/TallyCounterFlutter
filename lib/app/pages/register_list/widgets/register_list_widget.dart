af653558b6b11b1e69940cba10de9abb85b7940e
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-03-13 21:11:26 -0300

    :construction: feat: [WIP] Update list when delete using button
    Convert 'RegisterListWidget' to a StatefullWidget

--- lib/app/pages/register_list/widgets/register_list_widget.dart
+++ lib/app/pages/register_list/widgets/register_list_widget.dart
@@ -4,11 +4,24 @@ import 'package:tally_counter/app/core/domain/models/entities/counter_register.d
 
 import 'register_card.dart';
 
-class RegisterListWidget extends StatelessWidget {
+class RegisterListWidget extends StatefulWidget {
   final List<CounterRegister> registers;
 
   const RegisterListWidget({Key? key, required this.registers})
       : super(key: key);
+
+  @override
+  State<RegisterListWidget> createState() => _RegisterListWidgetState();
+}
+
+class _RegisterListWidgetState extends State<RegisterListWidget> {
+  var _registerList = const <CounterRegister>[];
+
+  @override
+  void initState() {
+    _registerList = widget.registers;
+    super.initState();
+  }
 
   String _formatDate(final DateTime date) {
     final formatter = DateFormat.MMMMEEEEd();
@@ -49,7 +62,17 @@ class RegisterListWidget extends StatelessWidget {
 
   VoidCallback _onDeleteCard(BuildContext context, final int index) {
     return () async {
-      _onDeleteSnackBar(context);
+      final originalList = _registerList;
+      _registerList = _registerList.toList();
+      setState(() {
+        _registerList.removeAt(index);
+      });
+      final canDelete = await _onDeleteSnackBar(context) ?? false;
+      if (!canDelete) {
+        setState(() {
+          _registerList = originalList;
+        });
+      }
     };
   }
 
@@ -59,66 +82,71 @@ class RegisterListWidget extends StatelessWidget {
       isAlwaysShown: true,
       child: ConstrainedBox(
         constraints: const BoxConstraints(maxWidth: 600),
-        child: ListView.separated(
+        child: ListView.builder(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-          itemCount: registers.length,
+          itemCount: _registerList.length,
           itemBuilder: (context, index) {
             return Dismissible(
-              key: ValueKey(registers[index]),
+              key: ValueKey(_registerList[index]),
               direction: DismissDirection.endToStart,
               dismissThresholds: const {
                 DismissDirection.endToStart: 0.6,
               },
               confirmDismiss: _canDeleteDismiss(context),
-              background: DecoratedBox(
-                decoration: const BoxDecoration(
-                  gradient: LinearGradient(
-                    colors: [Color(0x00ffffff), Color(0xf3a12a2a)],
+              background: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: DecoratedBox(
+                  decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                      colors: [Color(0x00ffffff), Color(0xf3a12a2a)],
+                    ),
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 8),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      RichText(
-                        text: TextSpan(
-                          text: 'Delete register from\n',
-                          children: [
-                            TextSpan(
-                              text: _formatDate(registers[index].startTime),
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 8),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            text: 'Delete register from\n',
+                            children: [
+                              TextSpan(
+                                text:
+                                    _formatDate(_registerList[index].startTime),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
-                            const TextSpan(text: ' at '),
-                            TextSpan(
-                              text: _formatTime(registers[index].startTime),
-                              style: const TextStyle(
-                                fontWeight: FontWeight.bold,
-                                fontStyle: FontStyle.italic,
+                              const TextSpan(text: ' at '),
+                              TextSpan(
+                                text:
+                                    _formatTime(_registerList[index].startTime),
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontStyle: FontStyle.italic,
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
-                      ),
-                      const Icon(
-                        Icons.delete_forever,
-                        size: 50,
-                      ),
-                    ],
+                        const Icon(
+                          Icons.delete_forever,
+                          size: 50,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
               onDismissed: (direction) {},
-              child: RegisterCardWidget(
-                counterRegister: registers[index],
-                onDeleteCallback: _onDeleteCard(context, index),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: RegisterCardWidget(
+                  counterRegister: _registerList[index],
+                  onDeleteCallback: _onDeleteCard(context, index),
+                ),
               ),
             );
-          },
-          separatorBuilder: (BuildContext context, int index) {
-            return const SizedBox(height: 8);
           },
         ),
       ),
