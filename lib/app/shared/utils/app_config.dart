e96c4f8efd3aeefee7243a0bbe95d598fc8152a2
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-06-18 01:33:39 -0300

    :comet: feat: [WIP] Create `appConfig` utility elements

---
+++ lib/app/shared/utils/app_config.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+import 'version_label.dart';
+
+enum _AppConfigKey { themeMode, dbVersion, clickDelayDuration }
+
+class AppConfig {
+  static const _defaultDuration = 5;
+  final ThemeMode theme;
+  final VersionLabel databaseVersion;
+  final Duration multiClickDuration;
+
+  const AppConfig(
+    this.databaseVersion, {
+    this.multiClickDuration = const Duration(milliseconds: _defaultDuration),
+    this.theme = ThemeMode.system,
+  });
+
+  Map<String, String> toJson() => {
+        _AppConfigKey.dbVersion.name: databaseVersion.toString(),
+        _AppConfigKey.clickDelayDuration.name:
+            multiClickDuration.inMilliseconds.toString(),
+        _AppConfigKey.themeMode.name: theme.name,
+      };
+
+  factory AppConfig.fromMap(Map<String, dynamic> map) {
+    final themeName = map[_AppConfigKey.themeMode.name] ?? '';
+    var theme = ThemeMode.system;
+    for (final ThemeMode mode in ThemeMode.values) {
+      if (mode.name == themeName) {
+        theme = mode;
+        break;
+      }
+    }
+
+    return AppConfig(
+      VersionLabel.fromString(map[_AppConfigKey.dbVersion.name] ?? 'v0.0.0'),
+      multiClickDuration: Duration(
+        milliseconds:
+            int.tryParse(map[_AppConfigKey.clickDelayDuration.name] ?? '') ??
+                _defaultDuration,
+      ),
+      theme: theme,
+    );
+  }
+}
---
+++ lib/app/shared/utils/version_label.dart
@@ -0,0 +1,84 @@
+import 'dart:typed_data';
+
+class VersionLabel {
+  static final zeroCharCode = '0'.codeUnitAt(0);
+  static final dotCharCode = '.'.codeUnitAt(0);
+
+  final int major;
+  final int mid;
+  final int minor;
+
+  const VersionLabel({this.major = 1, this.mid = 0, this.minor = 0});
+
+  factory VersionLabel.fromString(String number) {
+    var version = Uint8List(3);
+    var currentNumber = 0;
+    var numberIndex = 0;
+
+    final codeUnits = number.codeUnits;
+    for (var index = 0; index < codeUnits.length; index++) {
+      final character = codeUnits[index];
+      if (index > 0 && character == dotCharCode) {
+        if (numberIndex >= version.length) {
+          numberIndex = version.length - 1;
+          currentNumber =
+              (version[numberIndex] * 10) + character - zeroCharCode;
+        }
+        version[numberIndex] = currentNumber;
+        numberIndex += 1;
+        currentNumber = 0;
+      } else if (!(index == 0 && character == 'v'.codeUnitAt(0))) {
+        currentNumber = (currentNumber * 10) + character - zeroCharCode;
+      }
+    }
+    if (numberIndex < version.length) {
+      version[numberIndex] = currentNumber;
+    }
+
+    return VersionLabel(
+      major: version[0],
+      mid: version[1],
+      minor: version[2],
+    );
+  }
+
+  @override
+  String toString() {
+    return "v$major.$mid.$minor";
+  }
+
+  Uint8List asArray() {
+    return Uint8List.fromList([major, mid, minor]);
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is VersionLabel &&
+          runtimeType == other.runtimeType &&
+          major == other.major &&
+          mid == other.mid &&
+          minor == other.minor;
+
+  bool operator >(Object other) => other is VersionLabel && compare(other) > 0;
+  bool operator <(Object other) => other is VersionLabel && compare(other) < 0;
+
+  @override
+  int get hashCode => major.hashCode ^ mid.hashCode ^ minor.hashCode;
+
+  int compare(VersionLabel other) {
+    final sArr = asArray();
+    final oArr = other.asArray();
+
+    for (var index = 0; index < sArr.length; index++) {
+      if (sArr[index] > oArr[index]) {
+        return 1;
+      }
+      if (sArr[index] < oArr[index]) {
+        return -1;
+      }
+    }
+
+    return 0;
+  }
+}
