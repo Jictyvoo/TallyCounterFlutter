3db90548a2979ad3c3522eb7725779d3d3dfd5ce
João Victor Oliveira Couto <jictyvoo.ecomp@gmail.com>
2022-06-26 22:08:00 -0300

    :straight_ruler: style: [WIP] Add LayoutBuilder to home_page

--- lib/app/pages/counter/widgets/purpose_selector.dart
+++ lib/app/pages/counter/widgets/purpose_selector.dart
@@ -29,51 +29,95 @@ class _PurposeSelectorState extends State<PurposeSelector> {
     );
   }
 
+  Widget _buildHorizontalLayout() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      mainAxisSize: MainAxisSize.max,
+      children: [
+        const Expanded(
+          flex: 1,
+          child: SizedBox(
+            height: double.infinity,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: null,
+              child: Text('Add Purpose'),
+            ),
+          ),
+        ),
+        const VerticalDivider(
+          indent: 10,
+          endIndent: 10,
+        ),
+        Expanded(
+          flex: 2,
+          child: Scrollbar(
+            thumbVisibility: true,
+            child: ListView.builder(
+              itemCount: 11,
+              controller: ScrollController(),
+              physics: const BouncingScrollPhysics(),
+              itemBuilder: _itemBuilder,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildVerticalLayout() {
+    return Column(
+      children: [
+        Expanded(
+          flex: 8,
+          child: Scrollbar(
+            thumbVisibility: true,
+            child: CustomScrollView(
+              shrinkWrap: true,
+              controller: ScrollController(),
+              physics: const BouncingScrollPhysics(
+                parent: AlwaysScrollableScrollPhysics(),
+              ),
+              slivers: [
+                SliverFixedExtentList(
+                  delegate: SliverChildBuilderDelegate(
+                    _itemBuilder,
+                    childCount: 10,
+                  ),
+                  itemExtent: 50,
+                ),
+              ],
+            ),
+          ),
+        ),
+        const Expanded(
+          flex: 2,
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 4),
+            child: Align(
+              alignment: Alignment.center,
+              child: SizedBox(
+                width: double.maxFinite,
+                child: ElevatedButton(
+                  onPressed: null,
+                  child: Text('Add Purpose'),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 6),
-      child: Column(
-        children: [
-          Expanded(
-            flex: 8,
-            child: Scrollbar(
-              thumbVisibility: true,
-              child: CustomScrollView(
-                shrinkWrap: true,
-                physics: const BouncingScrollPhysics(
-                  parent: AlwaysScrollableScrollPhysics(),
-                ),
-                slivers: [
-                  SliverFixedExtentList(
-                    delegate: SliverChildBuilderDelegate(
-                      _itemBuilder,
-                      childCount: 10,
-                    ),
-                    itemExtent: 50,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          const Expanded(
-            flex: 2,
-            child: Padding(
-              padding: EdgeInsets.symmetric(vertical: 4),
-              child: Align(
-                alignment: Alignment.center,
-                child: SizedBox(
-                  width: double.maxFinite,
-                  child: ElevatedButton(
-                    onPressed: null,
-                    child: Text('Add Purpose'),
-                  ),
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxHeight >= 400 || constraints.maxWidth < 280) {
+        return _buildVerticalLayout();
+      }
+
+      return _buildHorizontalLayout();
+    });
   }
 }
