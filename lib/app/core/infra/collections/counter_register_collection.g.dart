1d35dedb4d192014655dfafb735a12304ed6881c
João Victor Oliveira Couto <commits@jictyvoo.dev>
2023-06-06 00:19:13 -0300

    :package: chore: Update Flutter version + dependencies

--- lib/app/core/infra/collections/counter_register_collection.g.dart
+++ lib/app/core/infra/collections/counter_register_collection.g.dart
@@ -7,7 +7,7 @@ part of 'counter_register_collection.dart';
 // **************************************************************************
 
 // coverage:ignore-file
-// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters
+// ignore_for_file: duplicate_ignore, non_constant_identifier_names, constant_identifier_names, invalid_use_of_protected_member, unnecessary_cast, prefer_const_constructors, lines_longer_than_80_chars, require_trailing_commas, inference_failure_on_function_invocation, unnecessary_parenthesis, unnecessary_raw_strings, unnecessary_null_checks, join_return_with_assignment, prefer_final_locals, avoid_js_rounded_ints, avoid_positional_boolean_parameters, always_specify_types
 
 extension GetTallyRegisterCollectionCollection on Isar {
   IsarCollection<TallyRegisterCollection> get tallyRegisterCollections =>
@@ -73,7 +73,7 @@ const TallyRegisterCollectionSchema = CollectionSchema(
   getId: _tallyRegisterCollectionGetId,
   getLinks: _tallyRegisterCollectionGetLinks,
   attach: _tallyRegisterCollectionAttach,
-  version: '3.0.5',
+  version: '3.1.0+1',
 );
 
 int _tallyRegisterCollectionEstimateSize(
